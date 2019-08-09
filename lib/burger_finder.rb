849b382e22f068dd8ebb6a6d4a3b7ef37b742404
scrabill <shannon.crabill@gmail.com>
2019-08-09 17:58:42 -0400

    Requires pry

--- lib/burger_finder.rb
+++ lib/burger_finder.rb
@@ -1,10 +1,13 @@
-require_relative "./burger_finder/version"
+require "nokogiri"
+require "open-uri"
+require "pry"
 
 module BurgerFinder
   class Error < StandardError; end
   # Your code goes here...
 end
 
+require_relative "./burger_finder/version"
 require_relative "./burger_finder/CLI"
 require_relative "./burger_finder/restaurants"
 require_relative "./burger_finder/scraper"
