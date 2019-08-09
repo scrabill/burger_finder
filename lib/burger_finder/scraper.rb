74b8e41e949f013050648b73b0ab1279c48a802f
scrabill <shannon.crabill@gmail.com>
2019-08-09 18:43:31 -0400

    Adds constants for scraper

--- lib/burger_finder/scraper.rb
+++ lib/burger_finder/scraper.rb
@@ -3,6 +3,25 @@ require "nokogiri"
 
 class Scraper
 
+  @@all = []
+
+  @@file = File.read("fixtures/The 15 Best Places for Burgers in Baltimore.htm")
+  @@doc = Nokogiri::HTML(@@file)
+
+
+  def self.scrape_page
+    # doc = Nokogiri::File(open(""))
+    # file = File.read("fixtures/The 15 Best Places for Burgers in Baltimore.htm")
+    # doc = Nokogiri::HTML(file)
+
+
+
+    doc.css("h2").first.text
+
+  end
+
+    binding.pry
+
   def self.main_page
     Nokogiri::HTML(open("https://foursquare.com/explore?mode=url&near=90210&q=Burger"))
   end
