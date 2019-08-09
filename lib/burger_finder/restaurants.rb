d6d954ac7d14df5dc301cd9bbbf25ccaf7611849
scrabill <shannon.crabill@gmail.com>
2019-08-09 17:35:01 -0400

    Stubs method for creating new restaurants and displaying them as a list

--- lib/burger_finder/restaurants.rb
+++ lib/burger_finder/restaurants.rb
@@ -3,6 +3,8 @@ require "nokogiri"
 
 class Restaurants
 
+attr_accessor :name, :rating, :cost, :neighborhood, :website
+
   # path = "https://www.yelp.com/search?cflt=burgers&find_loc=Downtown%2C+Baltimore%2C+MD"
 
   # Div = lemon--div__373c0__1mboc border-color--default__373c0__2xHhl
@@ -16,13 +18,55 @@ class Restaurants
   file = File.read('fixtures/test.htm')
   doc = Nokogiri::HTML(file)
 
-  @@all = {}
+  @@all = []
 
-  def initialize
-    puts "This is a test"
+  def self.list_burgers
+    # puts "Good Burger - $$"
+    #
+    Restaurants.new("Good Burger", "8,7", "$$", "Brooklyn", "https://en.wikipedia.org/wiki/Good_Burger")
+    Restaurants.new("Burger Kind", "3.4", "$", "Ocean City", "https://en.wikipedia.org/wiki/Good_Burger")
+    Restaurants.new("Bobby's Burger Palace", "9,9", "$$$", "Baltmore", "https://en.wikipedia.org/wiki/Good_Burger")
+
+
+    # puts "#{restaurant1.name} - #{restaurant1.rating} - #{restaurant1.cost} - #{restaurant1.neighborhood}"
+    # puts "#{restaurant2.name} - #{restaurant2.rating} - #{restaurant2.cost} - #{restaurant2.neighborhood}"
+    # puts "#{restaurant3.name} - #{restaurant3.rating} - #{restaurant3.cost} - #{restaurant3.neighborhood}"
+
+    # p restaurant1.name
+
+    @@all.uniq.each_with_index do |restaurant,i|
+      puts "#{i+1} -- #{restaurant.name} - #{restaurant.rating} - #{restaurant.cost} - #{restaurant.neighborhood}"
+    end
+
+  end
+
+  def initialize(name, rating, cost, neighborhood, website)
+
+    @name = name
+    @rating = rating
+    @cost = cost
+    @neighborhood = neighborhood
+    @website = website
+    # self.name = "Good Burger"
+    # self.rating = "8.7"
+    # self.cost = "$$"
+    # self.neighborhood = "Brooklyn"
+    # self.website = "https://en.wikipedia.org/wiki/Good_Burger"
+
+    @@all << self
+
+
+
+
+
+    # puts "This is a test"
     # doc.css("p.venueName a").text.each do |restaurant|
     #   puts "- #{restaurant}"
     # end
+  end
+
+  def self.all
+    @@all
   end
 
   # Resaurant Name: doc.css("p.venueName a").first.text
@@ -44,9 +88,4 @@ class Restaurants
   # Neighborhood: tipCount
   # Meta: doc.css("div.meta.adjusted").first.text (category, Neighborhood and number of tips)
 
-
-
-  def pull
-
-  end
 end
