f30ecddaa0b3745858925f66dad7da9f814000d3
scrabill <shannon.crabill@gmail.com>
2019-08-10 17:16:56 -0400

    Cleans scraper.rb file

--- lib/burger_finder/scraper.rb
+++ lib/burger_finder/scraper.rb
@@ -1,25 +1,19 @@
-require 'open-uri'
-require "nokogiri"
-
 class Scraper
-
-  @@all = []
 
   @@file = File.read("fixtures/The 15 Best Places for Burgers in Baltimore.htm")
   @@doc = Nokogiri::HTML(@@file)
-
 
   def self.scrape_main_page
 
     venues = @@doc.css("div.venueInfo")
     venues.each do |venue|
-      # puts venue.css("a.venueLink").text
 
       name = venue.css("a.venueLink").text
       rating = venue.css("span.venueScore span").text
       neighborhood = venues.css("span.tipCount").text.split(" · ")[1]
       website = venue.css("a.venueLink").attr("href").value
       venue = Restaurants.new(name, rating, neighborhood, website)
+
     end
 
     venues
@@ -51,31 +45,4 @@ class Scraper
 
   end
 
-  def self.main_page
-    Nokogiri::HTML(open("https://foursquare.com/explore?mode=url&near=90210&q=Burger"))
-  end
-
-  def self.all_results
-    self.main_page.css("div.venueDetails").each do |x|
-      puts x.text
-    end
-  end
-
-  def self.make_restaurants
-    self.all_results.each do |burger_joint|
-      puts array = burger_joint.gsub(". "," • ").split(" • ")
-      puts burger_joint.name = array[1]
-      puts burger_joint.rating = array[0]
-      puts burger_joint.location = array[1]
-      puts "***"
-    end
-  end
-
-  # attr_accessor :name, :see_burgers, :zip
-  #
-  # path = "https://foursquare.com/explore?mode=url&near=#{CLI.zip}&q=Burger"
-  #
-  # def initialize#(path)
-  #   puts "test"
-  # end
 end
