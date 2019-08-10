c505e12343ec5eb12874fd64098ea681b33dce68
scrabill <shannon.crabill@gmail.com>
2019-08-09 23:40:06 -0400

    Adds logic handling for invalid restaurant number input

--- lib/burger_finder/CLI.rb
+++ lib/burger_finder/CLI.rb
@@ -57,12 +57,25 @@ end
   end
 
   def menu
+    input = nil
+    while input == nil
+
     puts "To learn more about a restaurant, enter it's number"
 
-    input = gets.chomp.to_i
+    input = gets.chomp
 
-    puts "Restaurant ##{input}"
-    Restaurants.details(input)
+      if input.to_i > Restaurants.all.length
+        puts "I'm sorry, I did not understand your input"
+        # input = gets.chomp.to_i
+        menu
+      elsif input.to_i <= Restaurants.all.length
+        input = input.to_i
+        Restaurants.details(input)
+      else
+        puts "I'm sorry, I did not understand your input. Please enter a number between 1-15"
+      end
+
+    end
 
   end
 
