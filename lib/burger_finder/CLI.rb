33149a6b5af732194d4fe8bbd67b7a9765b12b6d
scrabill <shannon.crabill@gmail.com>
2019-08-10 17:40:10 -0400

    Consolidates CLI methods

--- lib/burger_finder/CLI.rb
+++ lib/burger_finder/CLI.rb
@@ -12,10 +12,7 @@ attr_accessor :name
   end
 
   def start
-
-    # Show the list of all restaurants
-    # Ask which restaurant they would like more info on
-    # Display the details for that restaurant
+    
     puts "Would you like to see a list of burger restuarants in your area? Enter Y or N to exit"
     input = gets.chomp
 
@@ -27,31 +24,24 @@ attr_accessor :name
       Restaurants.list_burgers
       puts "\n"
 
-      menu
+      puts "To learn more about a restaurant, enter it's number or N to exit"
+      puts "\n"
+
+      input = gets.chomp
+
+        if input != "N"
+          input = input.to_i
+          Restaurants.details(input)
+          puts "\n"
+          start
+        else
+          puts "Ok, maybe next time!"
+          puts "\n"
+        end
+
     elsif input == "N"
       puts "Ok, maybe next time!"
       puts "\n"
-    end
-
-
-  end
-
-  def menu
-
-    puts "To learn more about a restaurant, enter it's number or E to exit"
-    puts "\n"
-
-    input = gets.chomp
-
-    if input == "E"
-      puts "Goodbye!"
-      puts "\n"
-      exit
-    else
-      input = input.to_i
-      Restaurants.details(input)
-      puts "\n"
-      start
     end
 
   end
