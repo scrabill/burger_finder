33fda8611c4bf4ec337aca0c6043332a776ddad7
scrabill <shannon.crabill@gmail.com>
2019-08-10 01:33:37 -0400

    Refactors CLI.rb to remove excess code

--- lib/burger_finder/CLI.rb
+++ lib/burger_finder/CLI.rb
@@ -1,62 +1,27 @@
-require_relative './restaurants.rb'
-require "pry"
-
 class CLI
 
-attr_accessor :name, :see_burgers, :zip
+attr_accessor :name
 
-def initialize(name = nil, see_burgers = nil, zip = nil)
-  # start
-  puts "Hi! What's your name?"
-  @name = gets.chomp
+  def initialize(name = nil)
 
-  # puts "Hi #{@name}. Would you like to see a list of burger joints in Baltimore? y/n"
-  # @see_burgers = gets.chomp
+    puts "Hi! What's your name?"
+    @name = gets.chomp
+    puts "Alright, #{@name}. Here are the burger restaurants available in your area."
 
-  # puts "Alright, #{@name}. Where do you want to search? Please enter a zip code."
-  # @zip = gets.chomp
-  puts "Alright, #{@name}. Here are the burger restaurants available in your area."
+    start
 
-  Restaurants.list_burgers
+  end
 
-  menu
-end
+  def start
 
-  # def initialize
-  #   # start
-  #   greeting
-  #   self.name = name
-  #   self.see_burgers = see_burgers
-  #   self.zip = zip
-  # end
+    Restaurants.list_burgers
 
-  def greeting
-    # name = nil
-    # see_burgers = nil
-    # zip = nil
+    menu
 
-    # until
-      name != nil
-      puts "Hi! What's your name?"
-      name = gets.chomp
-
-      puts "Hi #{name}. Would you like to see a list of burger joints in Baltimore? y/n"
-      see_burgers = gets.chomp
-
-      puts "Alright, #{name}. Where do you want to search? Please enter a zip code."
-      zip = gets.chomp
-    # end
-
-
-
-    # if zip != nil
-    #   Restaurants.test()
-    # else
-    #   puts "ok, no burgs for you"
-    # end
   end
 
   def menu
+    
     input = nil
     while input == nil
 
